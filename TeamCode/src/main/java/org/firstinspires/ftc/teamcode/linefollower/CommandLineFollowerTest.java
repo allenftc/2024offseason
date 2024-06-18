b2e85ff736772a9c9398ee2961db53fdde9c21c7
jppboi <allenftc@outlook.com>
2024-06-17 22:18:50 -0700

    added otos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linefollower/CommandLineFollowerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linefollower/CommandLineFollowerTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.linefollower;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
